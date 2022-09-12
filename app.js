f0d75c4ba6e8b9ebadd53fac4d1afcc2348d7b90
said1854 <msaliogullari@gmail.com>
2022-09-12 17:46:25 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const cheerio = require('cheerio');
+const axios = require('axios');
+const port = 3000;
+const articles = [];
+
+
+app.get('/', (req, res) => {
+
+    axios.get('https://www.theguardian.com/international')
+        .then((response) => {
+            const html = response.data;
+            const $ = cheerio.load(html);
+
+            $('a:contains("climate")', html).each(function () {
+                const title = $(this).text();
+                const url = $(this).attr('href')
+                articles.push({
+                    title,
+                    url
+                })
+            })
+            res.json(articles)
+        }).catch((err) => {
+            console.log(err)
+        })
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`)
+})
